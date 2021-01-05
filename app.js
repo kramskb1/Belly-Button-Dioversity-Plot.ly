9e534d49324a4391b6dbf197ea8ba98af4edfb41
Benjamin Kramskoi <ben.kramskoi@gmail.com>
2021-01-04 22:04:09 -0500

    last commit

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ function dropdown () {
    });
    var sample = names[0];
    metadata(sample);
+   createchart(sample);
    });
   
 }
@@ -35,30 +36,68 @@ function metadata (dataid) {
             panel.append("h6").text(`${key}${value}`);
         }
         );
-        var trace = {
-            values: values,
-            labels: labels,
-            type: "pie",
-            text: hovers,
-            hoverinfo: "label+text+value+percent",
-            textinfo: "percent"
-        };
-        var data = [trace]
-        var layout = {
-            margin: {
-                l: 10,
-                r: 10,
-                b: 10,
-                t: 10,
-                pad: 4
-            }
-        }   
+        // var trace = {
+        //     values: values,
+        //     labels: labels,
+        //     type: "pie",
+        //     text: hovers,
+        //     hoverinfo: "label+text+value+percent",
+        //     textinfo: "percent"
+        // };
+        // var data = [trace]
+        // var layout = {
+        //     margin: {
+        //         l: 10,
+        //         r: 10,
+        //         b: 10,
+        //         t: 10,
+        //         pad: 4
+        //     }
+        // }   
   
-        Plotly.newPlot("pieChart", data, layout)
+        // Plotly.newPlot("pieChart", data, layout)
     });
-  };
-  const firstSample = sampleNames[0];
-  buildCharts(firstSample);
-  buildMetadata(firstSample);
-});
 
+};
+function optionChanged (newsample){
+    metadata(newsample);
+    createchart(newsample)
+}
+function createchart (sampleID){
+    d3.json("samples.json").then(function(data){
+        var chartsamples = data.samples;
+        var dataarray = chartsamples.filter(sample=>sample.id==sampleID);
+        var result = dataarray[0];
+        console.log(result);
+        var otu_ids = result.otu_ids;
+        var sample_values = result.sample_values;
+        var otu_labels = result.otu_labels;
+        var bubbledata = [{
+            x:otu_ids,
+            y:sample_values,
+            text:otu_labels,
+            mode:"markers",
+            marker:{
+                size:sample_values,
+                color:otu_ids
+            }
+        }];
+        var bubblelayout={
+            title:"Bacteria Culture per Sample"
+        }
+        Plotly.newPlot("bubble", bubbledata,bubblelayout);
+        var bardata=[{
+            x:sample_values.slice(0,10).reverse(),
+            y:otu_ids.slice(0,10).map(otu_ids=>`OTU ${otu_ids}`).reverse(),
+            text:otu_labels.slice(0,10).reverse(),
+            type:"bar",
+            orientation:"h"
+
+
+        }];
+        var barlayout={
+            title:"Top Ten Data Points for Bar Chart"
+        }
+        Plotly.newPlot("bar",bardata,barlayout)
+    });
+}
